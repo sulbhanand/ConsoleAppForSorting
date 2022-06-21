ce2cafdc08b84de79fd2fbf11239fc7b49281cdb
Sulbhanand-VIT <sulbhananddesai@gmail.com>
2022-06-22 01:14:03 +0530

    First Commit

---
+++ NumberParser/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberFile;
+            Console.Write("Enter the number for sorting and file name following with space e.g.(4,5,1,9,10,58,34,12,0 <space> XML):\n");
+            numberFile = Console.ReadLine();
+
+            Console.WriteLine("\n Your Input is {0}: \n", numberFile);
+
+            string numbers = numberFile.Split(' ')[0];
+
+            string file = numberFile.Split(' ')[numberFile.Split(' ').Length - 1];
+
+            int[] intArray = numbers.Split(',').Select(x => int.Parse(x)).ToArray();
+
+            Array.Sort(intArray);
+
+            Array.Reverse(intArray);
+
+            Console.Write(string.Join(",", intArray) + " " + file);
+            Console.ReadLine();
+        }
+    }
+}
