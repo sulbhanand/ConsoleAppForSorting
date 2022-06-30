956b2c4a0288a1e9cc20ba64c0d5da65bb15f92c
Sulbhanand-VIT <sulbhananddesai@gmail.com>
2022-07-01 00:46:06 +0530

    Final code changes with factory method

--- NumberParser/Program.cs
+++ NumberParser/Program.cs
@@ -1,11 +1,118 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+using System.Xml;
 
 namespace NumberParser
 {
+    public class JSONOBJECT
+    {
+        public int[] SortArray { get; set; }
+    }
+    public interface IFactory
+    {
+        string SortStringInDescending(string numbers);
+    }
+
+    public class XMLOUTPUT : IFactory
+    {
+        public string SortStringInDescending(string numbers)
+        {
+            string xmlReturn = string.Empty;
+            string path = "D:\\Test\\DesignWayTest\\ConsoleAppForSorting\\NumberParser\\Output\\NumberParser.xml";
+            int[] intArray = numbers.Split(',').Select(x => int.Parse(x)).ToArray();
+
+            Array.Sort(intArray);
+
+            Array.Reverse(intArray);
+
+            using (XmlWriter writer = XmlWriter.Create(path))
+            {
+                writer.WriteStartElement("Sort");
+                foreach (int item in intArray)
+                {
+                    writer.WriteElementString("Number", item.ToString());
+                }
+                writer.WriteEndElement();
+                writer.Flush();
+
+            }
+            XmlDocument doc = new XmlDocument();
+            doc.Load(path);
+            xmlReturn = doc.InnerXml;
+            return xmlReturn;
+        }
+    }
+
+    public class JSONOUTPUT : IFactory
+    {
+        public string SortStringInDescending(string numbers)
+        {
+            string path = "D:\\Test\\DesignWayTest\\ConsoleAppForSorting\\NumberParser\\Output\\NumberParser.json";
+
+            int[] intArray = numbers.Split(',').Select(x => int.Parse(x)).ToArray();
+
+            Array.Sort(intArray);
+
+            Array.Reverse(intArray);
+
+            string json = JsonConvert.SerializeObject(intArray);
+            File.WriteAllText(path, json);
+
+            return JsonConvert.SerializeObject(string.Join(",", intArray).Split(','));
+        }
+    }
+
+    public class TXTOUTPUT : IFactory
+    {
+        public string SortStringInDescending(string numbers)
+        {
+            string path = "D:\\Test\\DesignWayTest\\ConsoleAppForSorting\\NumberParser\\Output\\NumberParser.txt";
+            
+            int[] intArray = numbers.Split(',').Select(x => int.Parse(x)).ToArray();
+
+            Array.Sort(intArray);
+
+            Array.Reverse(intArray);
+
+
+            if (!File.Exists(path))
+                File.Create(path).Dispose();
+
+             using (TextWriter tw = new StreamWriter(path))
+             {
+                tw.WriteLine(string.Join(",", intArray));
+             }
+
+            return string.Join(",", intArray);
+        }
+    }
+
+    public abstract class Factory
+    {
+        public abstract IFactory FactoryMethod(string type);
+    }
+
+    public class ConcreteFactory : Factory
+    {
+        public override IFactory FactoryMethod(string type)
+        {
+            switch (type)
+            {
+                case "XML": return new XMLOUTPUT();
+                case "JSON": return new JSONOUTPUT();
+                case "TXT": return new TXTOUTPUT();
+                default: throw new ArgumentException("Invalid file format", "type");
+            }
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
@@ -20,20 +127,30 @@ namespace NumberParser
 
             string file = numberFile.Split(' ')[numberFile.Split(' ').Length - 1];
 
-            Console.Write(SortString(numbers) + " " + file);
-            Console.ReadLine();
-        }
+            Factory factory = new ConcreteFactory();
+            if (file == "TXT")
+            {
+                IFactory xmlOut = factory.FactoryMethod("TXT");
+                xmlOut.SortStringInDescending(numbers);
+            }
+            else if (file == "JSON")
+            {
+                IFactory jsonOut = factory.FactoryMethod("JSON");
+                Console.WriteLine(jsonOut.SortStringInDescending(numbers));
+                Console.ReadLine();
+            }
+            else if (file == "XML")
+            {
+                IFactory xmlOut = factory.FactoryMethod("XML");
+                Console.WriteLine(xmlOut.SortStringInDescending(numbers));
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Please Enter the vaild file format");
+                Console.ReadLine();
+            }
 
-        // Factory Mathod
-        private static string SortString(string numbers)
-        {
-            int[] intArray = numbers.Split(',').Select(x => int.Parse(x)).ToArray();
-
-            Array.Sort(intArray);
-
-            Array.Reverse(intArray);
-
-            return string.Join(",", intArray);
         }
     }
 }
